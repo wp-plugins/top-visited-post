e12cdecf1ae2fd55e10ba386c7cfe8d559553919
Webster.K <Webster.K@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-04 01:52:01 +0000

    top visited post - 2.0
    git-svn-id: https://plugins.svn.wordpress.org/top-visited-post/trunk@1104600 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: Webster.K
 Donate link:
 Tags: Top visited post, post, views
 Requires at least: 2.5
-Tested up to: 3.9.1
-Stable tag: 1.0
+Tested up to: 4.1.1
+Stable tag: 2.0
 
 == Description ==
 CZ: Zobrazuje na nástěnce nejnavštěvovanější články.
@@ -15,6 +15,8 @@ CZ:
 1. Uploadnout plugin na Váš blok, nebo nainstalovat přes správce pluginů.
 2. Aktivovat.
 3. Toď vše!
+4. Plugin po spuštění hodinu získává data než je v tabulce zobrazí (návštěvy nejsou hned)
+5. Od této chvíle plugin automaticky každou hodinu spouští aktualizaci tabulky
 
 EN:
 1. Upload the plugin to your blog or install via plugin management.
@@ -23,8 +25,20 @@ EN:
 
 == Screenshots ==
 1. Jak plugin vypadá (how to plugin look)
+2. Jak plugin vypadá od verze 2.0 (how to plugin look - v2.0)
 
 == Changelog ==
-1.0
- - První verze (First Release)
- - Zobrazuje nejnavštěvovanější články (Show top visited posts)
+= 2.0 =
+* Opraveno několik chyb v typech příspěvků
+* Nyní data čerpá z databáze
+* Zaveden cron, který jednou za hodinu aktualizuje data
+* Po instalaci se první data nyní po hodině zobrazí - plugin do té doby získává data podle návštěv za tu hodinu
+* Na příspěvek v tabulce lze kliknout pro zobrazení
+* Přidány celkové statistiky
+* Přidány statistiky za týden
+* Přidány statistiky za den
+* Přidány statistiky aktuálně přihlášeného uživatele, uživatel tedy vidí, který jeho příspěvek je nejnavštěvovanější
+
+= 1.0 =
+* První verze (First Release)
+* Zobrazuje nejnavštěvovanější články (Show top visited posts)
--- top_visited_post.php
+++ top_visited_post.php
@@ -3,7 +3,7 @@
 Plugin Name: Nejnavštěvovanější příspěvky/Top visited posts
 Plugin URI: http://jk-software.cz, phgame.cz
 Description: Plugin, který ukazuje, které příspěvky jsou nejnavštěvovanější
-Version: 1.0
+Version: 2.0
 Author: Webster.K
 Author URI: http://jk-software.cz, http://phgame.cz
 */
@@ -12,63 +12,139 @@ Author URI: http://jk-software.cz, http://phgame.cz
 
 function top_visited_post_install(){
 	global $wpdb;
-	mysql_query("DROP TABLE IF EXISTS ".$wpdb->prefix."plugin_pocitadlo");
+	//mysql_query("DROP TABLE IF EXISTS ".$wpdb->prefix."plugin_pocitadlo");
+	//mysql_query("DROP TABLE IF EXISTS ".$wpdb->prefix."plugin_pocitadlo_dnes");
+	//mysql_query("DROP TABLE IF EXISTS ".$wpdb->prefix."plugin_pocitadlo_tyden");
+	//mysql_query("DROP TABLE IF EXISTS ".$wpdb->prefix."plugin_pocitadlo_celek");
 
-	mysql_query("CREATE TABLE IF NOT EXISTS ".$wpdb->prefix."plugin_pocitadlo (id INT NOT NULL AUTO_INCREMENT,stranka TEXT NOT NULL,cislo INT(1) NOT NULL,PRIMARY KEY (id))");
-	
+	mysql_query("CREATE TABLE IF NOT EXISTS ".$wpdb->prefix."plugin_pocitadlo (id INT NOT NULL AUTO_INCREMENT,post_id INT NOT NULL,cas INT NOT NULL, cislo INT(1) NOT NULL,PRIMARY KEY (id))");
+	mysql_query("CREATE TABLE IF NOT EXISTS ".$wpdb->prefix."plugin_pocitadlo_dnes (id INT NOT NULL AUTO_INCREMENT,post_id INT NOT NULL,cas INT NOT NULL, cislo INT(1) NOT NULL,PRIMARY KEY (id))");
+	mysql_query("CREATE TABLE IF NOT EXISTS ".$wpdb->prefix."plugin_pocitadlo_tyden (id INT NOT NULL AUTO_INCREMENT,post_id INT NOT NULL,cas INT NOT NULL, cislo INT(1) NOT NULL,PRIMARY KEY (id))");
+	mysql_query("CREATE TABLE IF NOT EXISTS ".$wpdb->prefix."plugin_pocitadlo_celek (id INT NOT NULL AUTO_INCREMENT,post_id INT NOT NULL,cas INT NOT NULL, cislo INT(1) NOT NULL,PRIMARY KEY (id))");
 }
 	
 function top_visited_post_uninstall(){
-	global $wpdb;
-	mysql_query("DROP TABLE IF EXISTS ".$wpdb->prefix."plugin_pocitadlo");
+	//global $wpdb;
+	//mysql_query("DROP TABLE IF EXISTS ".$wpdb->prefix."plugin_pocitadlo");
 }
 
 function top_visited_post_dashboard_widgets_obsah() {
 	global $wpdb;
-	$pocet_zobrazeni = mysql_query("SELECT cislo, stranka FROM ".$wpdb->prefix."plugin_pocitadlo ORDER BY cislo DESC LIMIT 10") or die(mysql_error());
+	$pocet_zobrazeni = mysql_query("SELECT cislo, cas, post_id FROM ".$wpdb->prefix."plugin_pocitadlo_celek ORDER BY cislo DESC LIMIT 15") or die(mysql_error());
 	?>
 	<div class="top_visited_post_dashboard" style="width:100%">
-		<table>
+	<?php
+	if(isset($_POST["pocitadlo_submit"])){
+		echo "Tabulka byla obnovena<br>";
+	}
+	?>
+		<table width="100%"><tr><td colspan="2"><br><b>15 celkově nejnavštěvovanějších příspěvků na blogu:</b></td></tr><tr><td><b>Příspěvek</b></td><td><b>Počet zobrazení</b></td></tr><tr><td colspan="2"><hr></td></tr>
 	<?php
 	while($pocet_zobraz = mysql_fetch_array($pocet_zobrazeni)):
-		echo "<tr><td>" . $pocet_zobraz["stranka"] . "</td><td>". $pocet_zobraz["cislo"] ."</td></tr>";
+		echo "<tr><td><a href=\"". get_permalink($pocet_zobraz["post_id"]) ."\" target=\"_blank\">" . get_the_title($pocet_zobraz["post_id"]) . "</a></td><td width=\"50px\">". $pocet_zobraz["cislo"] ."</td></tr>";
 	endwhile;
+	
+	$pocet_zobrazeni_tyden = mysql_query("SELECT cislo, cas, post_id FROM ".$wpdb->prefix."plugin_pocitadlo_tyden ORDER BY cislo DESC ") or die(mysql_error());
+	echo "<tr><td colspan=\"2\"><br><br><b>10 nejnavštěvovanějších příspěvků za poslední týden:</b></td></tr>";
+	while($pocet_zobraz_tyden = mysql_fetch_array($pocet_zobrazeni_tyden)):
+		echo "<tr><td><a href=\"". get_permalink($pocet_zobraz_tyden["post_id"]) ."\" target=\"_blank\">" . get_the_title($pocet_zobraz_tyden["post_id"]) . "</a></td><td width=\"50px\">". $pocet_zobraz_tyden["cislo"] ."</td></tr>";
+	endwhile;
+		
+	$pocet_zobrazeni_dnes = mysql_query("SELECT cislo, cas, post_id FROM ".$wpdb->prefix."plugin_pocitadlo_dnes ORDER BY cislo DESC ") or die(mysql_error());
+	echo "<tr><td colspan=\"2\"><br><br><b>Dnešních 10 nejnavštěvovanějších příspěvků:</b></td></tr>";
+	while($pocet_zobraz_dnes = mysql_fetch_array($pocet_zobrazeni_dnes)):
+		echo "<tr><td><a href=\"". get_permalink($pocet_zobraz_dnes["post_id"]) ."\" target=\"_blank\">" . get_the_title($pocet_zobraz_dnes["post_id"]) . "</a></td><td width=\"50px\">". $pocet_zobraz_dnes["cislo"] ."</td></tr>";
+	endwhile;
+	
+	
+	echo "<tr><td colspan=\"2\"><br><br><b>Vaše nejnavštěvovanější příspěvky:</b></td></tr>";
+	$pocet_zobrazeni_user = mysql_query("SELECT cislo, cas, post_id FROM ".$wpdb->prefix."plugin_pocitadlo_celek ORDER BY cislo DESC") or die(mysql_error());
+	$i = 0;
+	while($pocet_zobraz_usr = mysql_fetch_array($pocet_zobrazeni_user)):
+		$zjisteni_id_autora = mysql_query("SELECT post_author FROM ".$wpdb->prefix."posts WHERE ID='".$pocet_zobraz_usr["post_id"]."'");
+		while($post_autor_id = mysql_fetch_array($zjisteni_id_autora)):
+			$user_id_ted = get_current_user_id();
+			if($user_id_ted == $post_autor_id["post_author"]){
+				//je autor, takze zobrazit
+				if($i<=10){
+					echo "<tr><td><a href=\"". get_permalink($pocet_zobraz_usr["post_id"]) ."\" target=\"_blank\">" . get_the_title($pocet_zobraz_usr["post_id"]) . "</a></td><td width=\"50px\">". $pocet_zobraz_usr["cislo"] ."</td></tr>";
+				}
+			}
+		endwhile;
+		$i++;
+	endwhile;
+	
 	?>
-	</table>
+	</table><br><br>
+	<!--
+	<form action="" method="post">
+	<input type="submit" value=" Aktualizovat tabulku" name="top_visited_post" class="button button-primary">
+	</form>
+	-->
 	</div>
 	<?php
 }
 
 function top_visited_post_dashboard_widgets() {
+	if(isset($_POST["top_visited_post"])){
+		//pridat funkci na aktualizaci
+		top_visited_post_aktualizace_tabulek();
+		
+		
+	}
 	wp_add_dashboard_widget(
                  'top_visited_post_dashboard_widget',
                  'Počítadlo',
                  'top_visited_post_dashboard_widgets_obsah'
         );	
 }
+
+if ( ! wp_next_scheduled( 'top_visited_post_task_hook' ) ) {
+  wp_schedule_event( time(), 'hourly', 'top_visited_post_task_hook' );
+}
+
+add_action( 'top_visited_post_task_hook', 'top_visited_post_aktualizace_tabulek' );
+
+function top_visited_post_aktualizace_tabulek() {
+	global $wpdb;
+	//aktualizace tabulek	
+	//celkem prispevky
+	mysql_query("TRUNCATE table ".$wpdb->prefix."plugin_pocitadlo_celek");
+	mysql_query("INSERT INTO ".$wpdb->prefix."plugin_pocitadlo_celek (post_id, cas, cislo) SELECT post_id, cas, sum(cislo) as cislo FROM ".$wpdb->prefix."plugin_pocitadlo GROUP BY post_id ORDER BY cislo DESC LIMIT 1000");
+	
+	//celkem tyden
+	$datum = StrFTime("%d/%m/%Y", current_time( 'timestamp', 0 ));
+	$retezec_pro_datum = explode("/",$datum);
+	$datum_na_strtime = mktime(0, 0, 0, $retezec_pro_datum[1], $retezec_pro_datum[0], $retezec_pro_datum[2]);
+	$datum_na_strtime_tyden = $datum_na_strtime - (7 * 24 * 3600);
+	mysql_query("TRUNCATE table ".$wpdb->prefix."plugin_pocitadlo_tyden");
+	mysql_query("INSERT INTO ".$wpdb->prefix."plugin_pocitadlo_tyden (post_id, cas, cislo) SELECT post_id, cas, sum(cislo) as cislo FROM ".$wpdb->prefix."plugin_pocitadlo WHERE cas>'".$datum_na_strtime_tyden."' GROUP BY post_id ORDER BY cislo DESC LIMIT 10");
+	
+	//celkem dnes
+	$datum_na_strtime_dnes = strtotime($datum);
+	mysql_query("TRUNCATE table ".$wpdb->prefix."plugin_pocitadlo_dnes");
+	mysql_query("INSERT INTO ".$wpdb->prefix."plugin_pocitadlo_dnes (post_id, cas, cislo) SELECT post_id, cas, sum(cislo) as cislo FROM ".$wpdb->prefix."plugin_pocitadlo WHERE cas>'".$datum_na_strtime."' GROUP BY post_id ORDER BY cislo DESC LIMIT 10");
+	
+	}
+
+
 	
 function top_visited_post_zobrazeni_na_strance() {
-
-	global $wpdb;
-	$adresa = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	if(is_single()){
+		$id = get_the_ID();
+		global $wpdb;
 	
-	$vyber_stranky = mysql_query("SELECT * FROM ".$wpdb->prefix."plugin_pocitadlo WHERE stranka='".$adresa."'");
-	$pocet_stranek = mysql_num_rows($vyber_stranky);
-	
-	if($pocet_stranek>=1){
-		//stranka je jiz vytvořena, stačí přidat hodnotu
-		while($vyber_hodnoty = mysql_fetch_array($vyber_stranky)):
-			$cislo = $vyber_hodnoty["cislo"];
-		endwhile;
-		$cislo = $cislo + 1;
-		mysql_query("UPDATE ".$wpdb->prefix."plugin_pocitadlo SET cislo='". $cislo ."' WHERE stranka='".$adresa."'");
-	}else{
+		$cas = current_time( 'timestamp', 0 );
 		//stranku je potřeba vytvořit
-		mysql_query("INSERT INTO ".$wpdb->prefix."plugin_pocitadlo (stranka, cislo) VALUES ('".$adresa."','1')");
-	}
+		mysql_query("INSERT INTO ".$wpdb->prefix."plugin_pocitadlo (post_id,cas,cislo) VALUES ('".$id."','".$cas."','1')");
+		?>
+		<!--  Top visited post is active - post ID: <?php echo $id;?> -->
+		<?php
+	}else{
 	?>
-	<!--  1  -->
+	<!--  Top visited post is active -->
 	<?php
+	}
 }
 
 add_action( 'wp_head', 'top_visited_post_zobrazeni_na_strance' );
